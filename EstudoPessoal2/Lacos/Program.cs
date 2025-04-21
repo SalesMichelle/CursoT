6a4e58e834362db1af857e06428dddda94626f69
SalesMichelle <michelle.santos@mottu.com.br>
2025-04-20 23:10:07 -0300

    Estudo do laço for

--- EstudoPessoal2/Lacos/Program.cs
+++ EstudoPessoal2/Lacos/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Desafio: Imprimir os números de 1 a 50, mas somente os múltiplos de 5 ou múltiplos de 7.
+
+using System ;
+
+class Program
+{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Números primos entre 1 e 100: ");
+
+            int i ;
+            for(i = 1; i <= 50; i ++)
+            {
+                if (i % 5 == 0 || i % 7 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+
+            }
+        }
+}
