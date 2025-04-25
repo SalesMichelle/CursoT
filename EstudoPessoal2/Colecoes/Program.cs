f32078cbe41dc563e45b0365a57d12d043c038a5
SalesMichelle <michelle.santos@mottu.com.br>
2025-04-24 22:29:44 -0300

    Estudo de Coleções

---
+++ EstudoPessoal2/Colecoes/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace Listas_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> valores = new List<int>();
+
+            valores.Add(16);
+            valores.Add(18);
+            valores.Add(23);
+            valores.Add(12);
+
+            int[] numeros = new int[] {32, 12, 7, 20};
+            valores.AddRange(numeros);
+
+            Console.WriteLine("A lista possui " + valores.Count() + " elementos");
+
+        }
+    }
+}
