58cbdbbfdd7bc52c66042e1260e4ef740d3eff3e
SalesMichelle <michelle.santos@mottu.com.br>
2025-04-27 21:25:14 -0300

    Estudo de Coleções até Stack

--- EstudoPessoal2/Colecoes2/Program.cs
+++ EstudoPessoal2/Colecoes2/Program.cs
@@ -1,24 +1,31 @@
-﻿using System;
+﻿// Você tem uma pilha de livros. A pilha é uma estrutura de dados onde o último livro colocado é o primeiro a ser retirado (LIFO - Last In, First Out).
+// Crie uma Stack (pilha) para armazenar nomes de livros.
+// Peça ao usuário para digitar 5 nomes de livros e adicione-os à pilha.
+// Após isso, mostre todos os livros armazenados na pilha, retirando-os um a um (usando o método Pop()), e exibindo cada um no console.
+
+using System;
 using System.Collections.Generic;
 
-namespace Listas_01
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Stack<string> pilha = new Stack<string>();
+
+        for(int i = 0 ; i < 5 ; i ++ )
         {
-            List<int> valores = new List<int>();
-
-            valores.Add(16);
-            valores.Add(18);
-            valores.Add(23);
-            valores.Add(12);
-
-            int[] numeros = new int[] {32, 12, 7, 20};
-            valores.AddRange(numeros);
-
-            Console.WriteLine("A lista possui " + valores.Count() + " elementos");
-
+            Console.WriteLine("Digite os livros em sequência");
+            string livro = Console.ReadLine(); // Lê o livro
+            pilha.Push(livro); // Adiciona à lista
         }
+        
+        // Mostrando os livros da pilha, retirando um a um
+        Console.WriteLine("Livros armazenados na pilha (ordem inversa): ");
+        
+        while(pilha.Count > 0)
+        {
+             Console.WriteLine(pilha.Pop()); // Retira e mostra o livro do topo da pilha.
+        }
+    
     }
 }
