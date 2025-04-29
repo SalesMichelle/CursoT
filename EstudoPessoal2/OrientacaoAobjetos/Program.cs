96e229203e326831eceb418b3f9e5f6f16fca5ba
SalesMichelle <michelle.santos@mottu.com.br>
2025-04-29 19:59:31 -0300

    Introdução ao Polimorfismo

--- EstudoPessoal2/OrientacaoAobjetos/Program.cs
+++ EstudoPessoal2/OrientacaoAobjetos/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+class Animal
+{
+    public virtual void Falar()
+    {
+        Console.WriteLine("Método padrão da classe Animal");
+    }
+}
+
+// Class dervidada
+
+class Cachorro : Animal
+{
+    public override void Falar()
+    {
+        Console.WriteLine("O cahorro faz Au Au");
+    }
+}
+
+class Gato : Animal
+{
+    public override void Falar()
+    {
+        Console.WriteLine("O gato faz miau!");
+    }
+}
+
+// Uso no programa
+
+class Program
+{
+    static void Main()
+    {
+        Animal cachorro = new Cachorro();
+        cachorro.Falar() ; // Saída desse comportamento
+
+        Animal gato = new Gato();
+        gato.Falar() ; // Saída desse comportamento
+    }
+}
+
